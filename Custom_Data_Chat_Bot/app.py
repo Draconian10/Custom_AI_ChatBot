d6bea8f604817b21ece4b4d9286f807bea45d870
Yash Pankhania <y.pankhania@somaiya.edu>
2023-12-21 20:19:48 -0500

    Integrated Pinecone Database

--- Custom_Data_Chat_Bot/app.py
+++ Custom_Data_Chat_Bot/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import langchain
-#from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
@@ -9,17 +9,22 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 import openai
-from langchain.llms import HuggingFaceHub
 import os
 from PyPDF2 import PdfReader
 from docx import Document
+import pinecone
+import time
+from langchain.vectorstores import Pinecone
 import toml
 
 def main():
-    #load_dotenv()
-    openai.api_key = st.secrets["OPENAI_API_KEY"]
+    # load_dotenv()
     st.set_page_config(page_title="Chat with multiple files", page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
+    openai.api_key = st.secrets["OPENAI_API_KEY"]
+    pinecone_api_key = st.secrets["PINECONE_API_KEY"]
+    pinecone_env = st.secrets["PINECONE_ENV"]
+    index_name = st.secrets["PINECONE_INDEX_NAME"]
 
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
@@ -38,7 +43,7 @@ def main():
     user_question = st.chat_input("What is up?")
     if user_question:
         if st.session_state.conversation is None:
-            st.error("Please upload files and click 'Process' before asking questions.")
+            st.error("Please provide data and click 'Process' before asking questions.")
         else:
             response = st.session_state.conversation({'question': user_question})
             st.session_state.chat_history = response['chat_history']
@@ -56,13 +61,23 @@ def main():
         uploaded_files = st.sidebar.file_uploader("Upload your files here and click on 'Process'", accept_multiple_files=True)
 
         if uploaded_files:
-            total_character_count = sum(len(file.read()) for file in uploaded_files)
-            if total_character_count > 400000:
-                st.warning("Total input data should not exceed 400,000 characters.")
-                st.stop()
-
             if st.sidebar.button("Process"):
                 with st.spinner("Processing"):
+
+                    # initialize pinecone
+                    pinecone.init(api_key=pinecone_api_key, environment=pinecone_env)
+
+                    if index_name in pinecone.list_indexes():
+                        pinecone.delete_index(index_name)
+
+                    # we create a new index
+                    pinecone.create_index(name=index_name, metric='cosine',
+                                          dimension=1536)  # 1536 dim of text-embedding-ada-002
+
+                    # wait for index to be initialized
+                    while not pinecone.describe_index(index_name).status['ready']:
+                        time.sleep(1)
+
                     st.session_state.conversation = None
                     st.session_state.chat_history = None
 
@@ -88,9 +103,9 @@ def main():
                     text_chunks = text_splitter.split_text(text)
 
                     embeddings = OpenAIEmbeddings()
-                    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
-
-                    llm = ChatOpenAI()
+                    # vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+                    vectorstore = Pinecone.from_texts(text_chunks, embeddings, index_name=index_name)
+                    llm = ChatOpenAI(model_name = 'gpt-4')
                     memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
                     conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vectorstore.as_retriever(), memory=memory)
                     st.session_state.conversation = conversation_chain
@@ -110,13 +125,30 @@ def main():
                 st.session_state.conversation = None
                 st.session_state.chat_history = None
 
+                # initialize pinecone
+                pinecone.init(api_key=pinecone_api_key, environment=pinecone_env)
+
+                if index_name in pinecone.list_indexes():
+                    pinecone.delete_index(index_name)
+
+                # we create a new index
+                pinecone.create_index(name=index_name, metric='cosine',
+                                      dimension=1536)  # 1536 dim of text-embedding-ada-002
+
+                # wait for index to be initialized
+                while not pinecone.describe_index(index_name).status['ready']:
+                    time.sleep(1)
+
+                st.session_state.conversation = None
+                st.session_state.chat_history = None
+
                 text_splitter = CharacterTextSplitter(separator="\n", chunk_size=1000, chunk_overlap=200, length_function=len)
                 text_chunks = text_splitter.split_text(user_text)
 
                 embeddings = OpenAIEmbeddings()
-                vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+                vectorstore = Pinecone.from_texts(text_chunks, embeddings, index_name=index_name)
 
-                llm = ChatOpenAI()
+                llm = ChatOpenAI(model_name = 'gpt-4')
                 memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
                 conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vectorstore.as_retriever(), memory=memory)
                 st.session_state.conversation = conversation_chain
